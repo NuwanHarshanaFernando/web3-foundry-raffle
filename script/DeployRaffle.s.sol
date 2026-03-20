0aa23d5589d0249a8573a1ae3b381bd6ba3c2b5c
NuwanHarshanaFernando <nuwanhfernando@gmail.com>
2026-03-20 12:45:50 +0530

    Added to Deploy Raffle Programatically

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
-import {CreateSubscription} from "script/Interactions.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "script/Interactions.s.sol";
 
 contract DeployRaffle is Script {
     function run() public {}
@@ -19,6 +19,11 @@ contract DeployRaffle is Script {
             // create subscription
             CreateSubscription createSubscription = new CreateSubscription();
             (config.subscriptionId, config.vrfCoordinator) = createSubscription.createSubscription(config.vrfCoordinator); // Saving the subscriptionId to config
+        
+            // Fund it
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(config.vrfCoordinator, config.subscriptionId, config.link);
+        
         }
 
         vm.startBroadcast();
@@ -31,6 +36,11 @@ contract DeployRaffle is Script {
             config.callbackGasLimit
         );
         vm.stopBroadcast();
+
+        AddConsumer addConsumer = new AddConsumer();
+        // Don't need to broadcast.... bcz AddConsumer has been broadcasted already.
+        addConsumer.addConsumer(address(raffle), config.vrfCoordinator, config.subscriptionId);
+
         return (raffle, helperConfig);
     }
 }
